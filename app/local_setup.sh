8b7e5f98883ddecdec18719d5c0dea27ef816e81
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2024-11-12 15:24:01 +0100

    Update README

--- app/local_setup.sh
+++ app/local_setup.sh
@@ -1,10 +1,7 @@
 #!/usr/local/bin/python
 echo yes | python manage.py collectstatic && \
 python manage.py compilemessages
-python manage.py makemigrations users projects dashboard && \
+python manage.py makemigrations users projects dashboard wefe && \
 python manage.py migrate && \
-python manage.py update_assettype && \
-python manage.py update_bmquestions && \
-python manage.py loaddata 'fixtures/multivector_fixture.json' && \
-python manage.py cp_setup && \
+python manage.py loaddata 'fixtures/fixture.json' && \
 echo 'Completed Setup Successfully!!'
