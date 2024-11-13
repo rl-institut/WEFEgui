58dbc0b12b8b2f03642c3abc37b71b5e8e401da1
paulapreuss <paulavpreuss@outlook.com>
2024-11-13 16:44:53 +0100

    Add pre-commit install to local setup

--- app/local_setup.sh
+++ app/local_setup.sh
@@ -4,4 +4,5 @@ python manage.py compilemessages
 python manage.py makemigrations users projects dashboard wefe && \
 python manage.py migrate && \
 python manage.py loaddata 'fixtures/fixture.json' && \
+pre-commit install && \
 echo 'Completed Setup Successfully!!'
