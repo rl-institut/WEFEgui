c93dec5558db6ecfd2c04aa22d844777970151eb
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-09-16 23:06:58 +0200

    Try to fix path

--- app/epa/settings.py
+++ app/epa/settings.py
@@ -15,19 +15,32 @@ import os
 
 from django.contrib.messages import constants as messages
 
+from pathlib import Path
+import sys
+import environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Root dir is a parent directory
+ROOT_DIR = environ.Path(__file__) - 2
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ast.literal_eval(os.getenv("DEBUG", "False"))
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (str(BASE_DIR / "static"),)
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "cdn_static_root")
+STATIC_ROOT = str(BASE_DIR / "cdn_static_root")
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+]
 
-STATICFILES_FINDERS = ["django.contrib.staticfiles.finders.FileSystemFinder"]
+
+
+
 
 if DEBUG is True:
     STATICFILES_FINDERS.append("sass_processor.finders.CssFinder")
